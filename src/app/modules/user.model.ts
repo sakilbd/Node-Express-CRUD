4df574be1e416dd05ebfad43e3e700b3462066b3
sakilbd <mdsakiluzzaman@gmail.com>
2023-11-23 17:19:35 +0600

    password incryption and hide from response done

--- src/app/modules/user.model.ts
+++ src/app/modules/user.model.ts
@@ -1,6 +1,10 @@
 import { Schema, model } from "mongoose";
 import { TUser } from "./users/user.interface";
 
+import bycrypt from 'bcrypt';
+import config from "../config";
+
+
 
 const userSchema = new Schema<TUser>({
     userId: { type: Number, required: true, unique: true },
@@ -25,6 +29,26 @@ const userSchema = new Schema<TUser>({
         quantity: { type: Number, required: true },
     }],
 });
+
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+    user.password = await bycrypt.hash(user.password, Number(config.bcrypt_salt_rounds))
+
+    next();
+})
+userSchema.post("save", function (doc, next) {
+    //actions after post data save ;
+})
+
+
+userSchema.methods.toJSON = function () {
+    const userObject = this.toObject();
+    delete userObject.password; // Remove password from the JSON object
+    return userObject;
+};
+
+
 
 
 export const UserModel = model<TUser>("User", userSchema);
